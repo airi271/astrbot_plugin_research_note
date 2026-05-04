95dadf27c783b1e2d04326a7ad0523ab1b9ca185
Airi271 <kokouuni@gmail.com>
2026-05-04 22:01:09 +0900

    Refactor ResearchNote plugin and update version in metadata

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+@register("ResearchNote", "airi271", "Research note assistant for source-grounded question answering.", "0.1.0")
+class ResearchNotePlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
@@ -19,6 +19,12 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    # 注册指令的装饰器。指令名为 research_hellow。注册成功后，发送 `/research_hellow` 就会触发这个指令，并回复 `Research Note is ready, {user_name}!`
+    @filter.command("research_hellow")
+    async def research_hellow(self, event: AstrMessageEvent):
+        """Research Note の動作確認コマンド。""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        user_name = event.get_sender_name()
+        yield event.plain_result(f"Research Note is ready, {user_name}!") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
